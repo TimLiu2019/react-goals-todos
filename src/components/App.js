b41070d79e1fca9ed03edb717d072f816905047f
TimLiu2019 <qitingliu7@gmail.com>
2021-04-29 11:51:47 -0400

    components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,32 @@
-
+import React, { Component } from "react";
 import "../App.css";
+import ConnectedTodos from "./Todos";
+import ConnectedGoals from "./Goals";
+import { connect } from "react-redux";
+import { handleIntialData } from "../actions/shared";
 
-function App() {
-  return <div>Hello World</div>;
+class App extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+
+    dispatch(handleIntialData());
+
+    // store.subscribe(() => this.forceUpdate());
+  }
+
+  render() {
+    if (this.props.loading === true) {
+      return <h3>Loading</h3>;
+    }
+    return (
+      <div>
+        <ConnectedTodos />
+        <ConnectedGoals />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(state => ({
+  loading: state.loading
+}))(App);
